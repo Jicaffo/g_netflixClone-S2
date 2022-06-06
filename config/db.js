ec76fc0a7e0db0b475879c35f6c1bd96b1ac046f
Jicaffo <jicaffo@yahoo.com>
2022-06-06 13:31:53 -0300

    postList route & controller added, small changes.

--- config/db.js
+++ config/db.js
@@ -5,21 +5,16 @@ dotenv.config({ path: '.env'})
 
 const connectDB = async () => {
     try {
-        //await mongoose.connect(process.env.DB_URL
-            await mongoose.connect(process.env.DB_URL
-              
-            , {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            
-        }
-        
-        )                          //PROBLEM
+        await mongoose.connect(process.env.DB_URL, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,    
+            }
+        )
         console.log('DB is working')
     } catch (error) {
+        console.log('Something went wrong with the DB Connection...')
         console.log(error)
-        console.log('there is something wrong')
-        process.exit(1) //if there is an error, the app will stopped
+        process.exit(1) //if there is an error, the app will stop
     }
 }
 
